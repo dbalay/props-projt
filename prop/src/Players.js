2c33ad342bf1d5fbadb0b0e43040a8be2442fe21
dbalay <daadbenalaya76@gmail.com>
2023-11-18 15:00:05 +0100

    first commit

---
+++ prop/src/Players.js
@@ -0,0 +1,34 @@
+import pho from './example-player-2.png'
+
+
+const Data = [{
+"id":'1',
+"Name":"khaled",
+"Team":'Real madrid',
+"Nationality":'tunisian',
+"iconeNatio":'https://cdn-icons-png.flaticon.com/512/4060/4060435.png',
+"jerseyNumber":'7',
+"Age":'32',
+"image":pho
+},
+{
+    "id":'2',
+    "Name":"Rami",
+    "Team":'Real madrid',
+     "Nationality":'tunisian',
+     "iconeNatio":'https://cdn-icons-png.flaticon.com/512/4060/4060435.png',
+    "jerseyNumber":'9',
+    "Age":'30',
+    "image":pho},
+    {
+        "id":'3',
+        "Name":"Ahmed",
+        "Team":'Fc barcelone',
+         "Nationality":'tunisian',
+         "iconeNatio":'https://cdn-icons-png.flaticon.com/512/4060/4060435.png',
+        "jerseyNumber":'8',
+        "Age":'29',
+        "image":pho
+    }
+]
+export default Data
